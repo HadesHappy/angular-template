9ae14bb157b7743bf3b73705000c91f09a242a19
HadesHappy <happyhades123@gmail.com>
2018-10-27 16:11:25 -0700

    nowy komponent

---
+++ src/app/nowy/nowy.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nowy',
+  templateUrl: './nowy.component.html',
+  styleUrls: ['./nowy.component.css']
+})
+export class NowyComponent implements OnInit {
+  sekret = "abra-kadabra";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ukryj() {
+    this.sekret = "********";
+  }
+}
